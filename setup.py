683c08270056364b7cccb2b7c8c6077afae36676
Tuomas Suutari <tuomas.suutari@anders.fi>
2017-05-16 12:22:04 +0300

    Bump version to 0.4.4.post0+hel
    Created a private release before these changes are in upstream.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-oidc-provider',
-    version='0.4.4',
+    version='0.4.4.post1+hel',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
